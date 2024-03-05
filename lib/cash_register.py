c7a8fd018dae1faa919799c79ba60d0b135a923e
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-05 20:15:41 +0300

    Partially complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+
+  def add_item(self, title, price, quantity=1):
+     self.total += price * quantity
+     self.items.extend([title] * quantity)
+  
+  def apply_discount(self):
+    if self.discount > 0:
+      self.total -= (self.total * self.discount) // 100
+      if self.total == 0:
+        print(f"After the discount, the total comes to $0.")
+      else:
+        print(f"After the discount, the total comes to ${self.total}")
+    else:
+      print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    if self.items:
+        last_item_title = self.items[-1]
+        last_item_price = self.get_price(last_item_title)
+        self.total -= last_item_price
+        self.items.pop()
+    else:
+      return self.total
+
+
+
